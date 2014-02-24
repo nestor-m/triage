56b40af452a136c78f7182f0afe3eabda67dce00
tejedamarcia@gmail.com <tejedamarcia@gmail.com>
2014-02-24 00:25:43 -0300

    Seteo como default el idioma es

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,7 @@
 // Place your Spring DSL code here
 beans = {
+	localeResolver(org.springframework.web.servlet.i18n.SessionLocaleResolver) {
+		defaultLocale = new Locale("es","ES")
+		java.util.Locale.setDefault(defaultLocale)
+	}		
 }
